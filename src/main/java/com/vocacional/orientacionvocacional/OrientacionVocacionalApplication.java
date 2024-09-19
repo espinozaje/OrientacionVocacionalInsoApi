94e9985578131f52b68366994f8c13fbcb6d070b
Espinozaje <jeisson12aaron@gmail.com>
2024-09-19 14:12:21 -0500

    first commit

---
+++ src/main/java/com/vocacional/orientacionvocacional/OrientacionVocacionalApplication.java
@@ -0,0 +1,13 @@
+package com.vocacional.orientacionvocacional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrientacionVocacionalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrientacionVocacionalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vocacional/orientacionvocacional/OrientacionVocacionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vocacional.orientacionvocacional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrientacionVocacionalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
